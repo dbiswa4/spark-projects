f5ae0a48ee7863595eb7b6d8cc9ebb0863516c52
Dipankar Biswas <dbiswas@iu.edu>
2016-08-07 22:26:26 -0700

    music profiling added

--- taming-spark-python/src/getting-started-spark/bid_analysis.py
+++ taming-spark-python/src/getting-started-spark/bid_analysis.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext, Row
+from pyspark.sql.types import *
 
 '''
 Ref:
@@ -8,8 +9,130 @@ http://stackoverflow.com/questions/28782940/load-csv-file-with-spark
 => For now, let's read it as text file and split based in comma
 '''
 
-def analyze_bid_data(sc, sqlContext):
+def bid_data_quick_look(sc):
     print 'analyze_bid_data()'
+    ebay_text = sc.textFile("/Users/dbiswas/Documents/misc/public_datasets/ebay/Cartier+7-day+auctions.csv")
+    print 'type of ebay_text : ', type(ebay_text)
+    print 'rows : ', ebay_text.first()
+    print 'ebay_text.first() return type : ', type(ebay_text.first())
+
+    ebay = ebay_text.filter(lambda line : "auctionid" not in line).map(lambda line : line.split(",")) \
+        .map(lambda line : (int(line[0]), int(line[1]), float(line[2]), line[3].encode("utf8"), \
+                            int(line[4]), int(line[5]), int(line[6])))
+    print 'type of ebay', type(ebay)
+    print 'rows : ', ebay.first()
+
+
+def bid_data_analysis(sc, sqlContext):
+    print '\n In bid_data_analysis()...'
+    ebay_text = sc.textFile("/Users/dbiswas/Documents/misc/public_datasets/ebay/Cartier+7-day+auctions.csv")
+    ebayTemp = ebay_text.filter(lambda line: "auctionid" not in line).map(lambda line: line.split(","))
+
+    print 'First row of ebayTemp : ', ebayTemp.first()
+    print 'type of ebayTemp : ', type(ebayTemp)
+
+    ebay = ebayTemp.map(lambda line : (int(line[0]), float(line[1]), float(line[2]), line[3].encode("utf8"), \
+                            float(line[4]), float(line[5]), float(line[6])))
+
+    '''
+    +---------+----+-------+-----------------+----------+-------+-----+
+    |auctionid| bid|bidtime|           bidder|bidderrate|openbid|price|
+    +---------+----+-------+-----------------+----------+-------+-----+
+    |     null|null|   null|        kona-java|      null|   null| null|
+    |     null|null|   null|           doc213|      null|   null| null|
+
+    Note:
+    If we create RDD as shown below, all the fields will be emitted as String. When you then try to attach the schema
+    shown below, type cast from String to Integer will fail.
+    '''
+    #ebay = ebayTemp.map(lambda line : (line[0], line[1], line[2], line[3], \
+    #                        line[4], line[5], line[6]))
+
+
+    print '\nFirst row of ebay : ', ebay.first()
+    print 'type of ebay : ', type(ebay)
+
+    schema = StructType([
+        StructField("auctionid", IntegerType()),
+        StructField("bid", FloatType()),
+        StructField("bidtime", FloatType()),
+        StructField("bidder", StringType()),
+        StructField("bidderrate", FloatType()),
+        StructField("openbid", FloatType()),
+        StructField("price", FloatType()),
+    ])
+
+    '''
+    Spark SQL supports automatically converting an "RDD containing case classes" to a DataFrame with the method toDF():
+    // change ebay RDD of Auction objects to a
+    val auction = ebay.toDF()
+
+    Let's do it in Python way here.
+    '''
+    auction = sqlContext.createDataFrame(ebay, schema)
+
+    #Shows first 20 rows
+    auction.show()
+    #Print Schema
+    auction.printSchema()
+
+    auction.select(auction['auctionid']).show()
+
+    '''
+    auction.select(auction['auctionid']).distinct.count()
+    => It does not work in Python. Looks like SQL APIs are not strong in Python
+    => It works in Scala
+    => Let's run direct SQL
+    '''
+
+    auction.registerTempTable("auction")
+
+    a = sqlContext.sql("select auctionid from auction limit 5")
+    a.show()
+
+    a = sqlContext.sql("select distinct auctionid from auction limit 5")
+    a.show()
+
+    a = sqlContext.sql("select count(*) from auction")
+    a.show()
+
+    a = sqlContext.sql("select count(auctionid) from auction")
+    a.show()
+
+    a = sqlContext.sql("select count(distinct auctionid) from auction")
+    a.show()
+
+    a = sqlContext.sql("select count(*) from (select distinct auctionid from auction) a")
+    a.show()
+
+    '''
+    Let's go back to DataFrame methods
+    Scala Reference:
+    // How many bids per item?  ** There is no column item in data. Seems to be typo
+    auction.groupBy("auctionid", "item").count.show
+
+    // What's the min number of bids per item?
+    // what's the average? what's the max?
+    auction.groupBy("item", "auctionid").count
+    .agg(min("count"), avg("count"),max("count")).show
+    '''
+    #How many bid put by a user in one auction process?
+    results = sqlContext.sql("SELECT auctionid, bidder, count(bid) FROM auction GROUP BY auctionid, bidder")
+    results.show()
+
+    #auctionid=1638843936
+    results = sqlContext.sql("SELECT auctionid, MAX(price) FROM auction GROUP BY auctionid")
+    results.show()
+
+    '''
+    !!! Looks like MIN() does not work !!!
+    '''
+    results = sqlContext.sql("SELECT auctionid, MIN(price) FROM auction GROUP BY auctionid")
+    results.show()
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -18,4 +141,5 @@ if __name__ == '__main__':
     sc = SparkContext(conf=sparkConf)
     sqlContext = SQLContext(sc)
 
-    analyze_bid_data(sc, sqlContext)
+    #bid_data_quick_look(sc)
+    bid_data_analysis(sc, sqlContext)
---
+++ taming-spark-python/src/getting-started-spark/cust_music_profile.py
@@ -0,0 +1,172 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext, Row
+from pyspark.mllib.stat import Statistics
+import csv
+
+'''
+Note:
+https://www.mapr.com/ebooks/spark/05-apache-spark-use-case-user-profiles.html
+'''
+
+def make_tracks_kv(str):
+    l = str.split(",")
+    '''
+    Note:
+    Pay attention to [] used as opposed to ().
+    [x,[[y, z]]]
+    Apprently one extra pair of [] has been used around [y, z]. To my understanding,
+    it is to tell Spark that there will be Array of [y, z] which it self is an Array.
+    That means Array of Array.
+    If we do not use additional pair of [], reduceByKey() operation just creates one
+    single comma separated string out of all possible values of [y, z].
+    We get similar effect if we use () braces
+    '''
+    return [l[1], [[int(l[2]), l[3], int(l[4]), l[5]]]]
+
+def make_tracks_kv_other(str):
+    l = str.split(",")
+    '''
+    Does produce a big long string of all the values for a key when used reduceByKey()
+    '''
+    return (l[1], ((int(l[2]), l[3], int(l[4]), l[5])))
+
+
+'''
+=> Average number of tracks during each period of the day (time ranges are arbitrarily defined in the code)
+=> Total unique tracks, i.e., the set of unique track IDs
+=> Total mobile tracks, i.e., tracks played when the mobile flag was set
+'''
+def compute_stats_byuser(tracks):
+    mcount = morn = aft = eve = night = 0
+    tracklist = []
+    for t in tracks:
+        trackid, dtime, mobile, zip = t
+
+        #Keeing track of unique tracks customer listened
+        if trackid not in tracklist:
+            tracklist.append(trackid)
+
+        d, t = dtime.split(" ")
+        hourofday = int(t.split(":")[0])
+        mcount += mobile
+
+        if(hourofday < 5):
+            night += 1
+        elif (hourofday < 12):
+            morn += 1
+        elif (hourofday < 17):
+            aft += 1
+        elif (hourofday < 22):
+            eve += 1
+        else:
+            night += 1
+
+    return [len(tracklist), morn, aft, eve, night, mcount]
+
+
+
+if __name__ == '__main__':
+    #sparkConf = SparkConf().setMaster("local[*]").setAppName("Music Profile")
+    sparkConf = SparkConf().setAppName("Music Profile")
+    sc = SparkContext(conf=sparkConf)
+
+    trackfile = sc.textFile('/Users/dbiswas/Documents/misc/public_datasets/music/tracks.csv')
+
+    #make a k, v RDD out of the input data
+    tbycust = trackfile.map(lambda line: make_tracks_kv(line))
+    tbycust_kv = tbycust.reduceByKey(lambda a, b: a + b)
+
+    '''
+    type of tbycust <class 'pyspark.rdd.PipelinedRDD'>
+    tbycust :  [u'48', [[453, u'"2014-10-23 03:26:20"', 0, u'"72132"']]]
+    type of tbycust_kv :  <class 'pyspark.rdd.PipelinedRDD'>
+    (u'4446', [[511, u'"2014-12-04 03:35:27"', 1, u'"23081"'], [527, u'"2014-11-26 21:31:06"', 0, u'"96142"']])
+    '''
+
+    print 'type of tbycust', type(tbycust)
+    print 'tbycust : ', tbycust.first()
+    print 'type of tbycust_kv : ', type(tbycust_kv)
+    print 'tbycust_kv : ', tbycust_kv.first()
+
+    '''
+    Experiment with () braces
+    '''
+    #make a k, v RDD out of the input data
+    #tbycust = trackfile.map(lambda line: make_tracks_kv_other(line))
+    #tbycust_kv = tbycust.reduceByKey(lambda a, b: a + b)
+
+    '''
+    ***
+    type of tbycust <class 'pyspark.rdd.PipelinedRDD'>
+    tbycust :  (u'48', (453, u'"2014-10-23 03:26:20"', 0, u'"72132"'))
+    type of tbycust_kv :  <class 'pyspark.rdd.PipelinedRDD'>
+    (u'4446', (511, u'"2014-12-04 03:35:27"', 1, u'"23081"', 527, u'"2014-11-26 21:31:06"', 0, u'"96142"'))
+    '''
+
+    #print '\n***\ntype of tbycust', type(tbycust)
+    #print 'tbycust : ', tbycust.first()
+    #print 'type of tbycust_kv : ', type(tbycust_kv)
+    #print 'tbycust_kv : ', tbycust_kv.first()
+
+    #compute profile for each user
+    custdata = tbycust_kv.mapValues(lambda a: compute_stats_byuser(a))
+
+    '''
+    custdata :  (u'4446', [103, 26, 25, 26, 30, 53])
+    type of custdata :  <type 'tuple'>
+    '''
+    print 'custdata : ', custdata.first()
+    print 'type of custdata : ', type(custdata.first())
+
+    count = 0
+
+    for profile in custdata.collect():
+
+        '''
+        profile[0] :  3724
+        type of profile[0] <type 'unicode'>
+        profile[1] :  [102, 29, 24, 26, 34, 60]
+        type of profile[1] <type 'list'>
+        profile[1][0] :  102
+        profile[1][1] :  29
+        '''
+        print 'profile[0] : ', profile[0]
+        print 'type of profile[0]', type(profile[0])
+        print 'profile[1] : ', profile[1]
+        print 'type of profile[1]', type(profile[1])
+        print 'profile[1][0] : ', profile[1][0]
+        print 'profile[1][1] : ', profile[1][1]
+        count += 1
+        if count > 2:
+            break
+
+    # compute aggregate stats for entire track history
+    aggdata = Statistics.colStats(custdata.map(lambda x: x[1]))
+
+    '''
+    type of aggdata :  <class 'pyspark.mllib.stat._statistics.MultivariateStatisticalSummary'>
+    '''
+    print 'type of aggdata : ', type(aggdata)
+
+    #custdata.saveAsTextFile("/Users/dbiswas/Documents/SourceCode/Spark/spark-projects/taming-spark-python/output/live_table.txt")
+
+    for k, v in custdata.collect():
+        unique, morn, aft, eve, night, mobile = v
+        tot = morn + aft + eve + night
+
+        # persist the data, in this case write to a file
+        with open('/Users/dbiswas/Documents/SourceCode/Spark/spark-projects/taming-spark-python/output/live_table.csv', 'a') as csvfile:
+            fwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            fwriter.writerow((k, tot, unique, morn, aft, eve, night, mobile))
+
+        # do the same with the summary data
+        with open('/Users/dbiswas/Documents/SourceCode/Spark/spark-projects/taming-spark-python/output/agg_table.csv', 'a') as csvfile:
+            fwriter = csv.writer(csvfile, delimiter=' ',
+                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            fwriter.writerow(Row(aggdata.mean()[0], aggdata.mean()[1],
+                             aggdata.mean()[2], aggdata.mean()[3], aggdata.mean()[4],
+                             aggdata.mean()[5]))
+
+
+
+
